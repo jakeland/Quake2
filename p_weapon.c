554accd68df6758a55a125f4202aaec514235fb0
jakeland <jakemc@embarqmail.com>
2014-02-05 20:33:06 -0500

    4 Rockets. Rockets are for Squares.

--- p_weapon.c
+++ p_weapon.c
@@ -763,6 +763,37 @@ void Weapon_RocketLauncher_Fire (edict_t *ent)
 	P_ProjectSource (ent->client, ent->s.origin, offset, forward, right, start);
 	fire_rocket (ent, start, forward, damage, 650, damage_radius, radius_damage);
 
+	
+        AngleVectors (ent->client->v_angle, forward, right, NULL);
+        
+        VectorScale (forward, -2, ent->client->kick_origin);
+        ent->client->kick_angles[0] = -1;
+
+        VectorSet(offset, 8, 0, ent->viewheight-8);
+        P_ProjectSource (ent->client, ent->s.origin, offset, forward, right, start);
+        fire_rocket (ent, start, forward, damage, 650, damage_radius, radius_damage);
+
+		AngleVectors (ent->client->v_angle, forward, right, NULL);
+        
+        VectorScale (forward, -2, ent->client->kick_origin);
+        ent->client->kick_angles[0] = -1;
+
+        VectorSet(offset, 8, 0, ent->viewheight);
+        P_ProjectSource (ent->client, ent->s.origin, offset, forward, right, start);
+        fire_rocket (ent, start, forward, damage, 650, damage_radius, radius_damage);
+
+		  AngleVectors (ent->client->v_angle, forward, right, NULL);
+        
+        VectorScale (forward, -2, ent->client->kick_origin);
+        ent->client->kick_angles[0] = -1;
+
+        VectorSet(offset, 8, 8, ent->viewheight);
+        P_ProjectSource (ent->client, ent->s.origin, offset, forward, right, start);
+        fire_rocket (ent, start, forward, damage, 650, damage_radius, radius_damage);
+        
+        
+
+
 	// send muzzle flash
 	gi.WriteByte (svc_muzzleflash);
 	gi.WriteShort (ent-g_edicts);
